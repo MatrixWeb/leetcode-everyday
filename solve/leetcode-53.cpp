e73dd9d9975795597666e07505e2c0f8c33a1234
matrixlin <matrixlin@tencent.com>
2020-05-03 12:38:54 +0800

    add leetcode 53

---
+++ solve/leetcode-53.cpp
@@ -0,0 +1,19 @@
+//dp
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1)
+            return nums[0];
+        vector<int> dp(n);
+        dp[0]=nums[0];
+        int ans = dp[0];
+
+        for(int i=1;i<n;i++)
+        {
+            dp[i]=max(nums[i],dp[i-1]+nums[i]);
+            ans = max(ans, dp[i]);
+        }
+        return ans;
+    }
+};
